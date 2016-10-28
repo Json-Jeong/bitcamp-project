a3ad7530d77ecec83de80c63a67ee7150261b204
JSON.Jeong <JSON.Jeong>
2016-10-28 11:09:09 +0900

    ex03

--- src/bitcamp/java89/ems/EduApp.java
+++ src/bitcamp/java89/ems/EduApp.java
@@ -3,14 +3,59 @@ package bitcamp.java89.ems;
 import java.util.Scanner;
 
 public class EduApp {
+  static Teacher[] teachers = new Teacher[100];
+  static int length = 0;
+  static Scanner keyScan = new Scanner(System.in);
+
   public static void main(String[] args) {
     System.out.println("비트캠프 관리시스템에 오신 걸 환영합니다.");
 
-    Teacher[] teachers = new Teacher[100];
-    int length = 0;
 
-    Scanner keyScan = new Scanner(System.in);
+    loop:
+    while (true) {
+      System.out.print("명령> ");
+      String command = keyScan.nextLine().toLowerCase();
 
+      switch (command) {
+        case "add" : doAdd(); break;
+        case "list" : doList(); break;
+        case "view" : doView(); break;
+        case "quit" :
+          System.out.println("Good bye!");
+          break loop;
+        default :
+          System.out.println("지원하지 않는 명령어입니다.");
+      }
+    }
+  }
+
+  static void doView() {
+    System.out.print("검색어(이름)> ");
+    String name = keyScan.nextLine().toLowerCase();
+    for (int i = 0; i < length; i++) {
+      if (teachers[i].name.toLowerCase().equals(name)) {
+        System.out.printf("강사 이름: %s\n", teachers[i].name);
+        System.out.printf("강의 분야: %s\n", teachers[i].language);
+        System.out.printf("강의 경력: %s\n", teachers[i].career);
+        System.out.printf("출간 저서: %s\n", teachers[i].writing);
+        break;
+      }
+    }
+  }
+
+  static void doList() {
+    for (int i = 0; i < length; i++) {
+      Teacher teacher = teachers[i];
+
+      System.out.printf("%s, %s, %s, %s\n",
+      teacher.name,
+      teacher.language,
+      teacher.career,
+      teacher.writing);
+    }
+  }
+
+  static void doAdd() {
     while (length < teachers.length) {
       Teacher teacher = new Teacher();
 
@@ -29,19 +74,6 @@ public class EduApp {
       if (!keyScan.nextLine().equals("y")) {
         break;
       }
-    }
-    printTeacherList(teachers, length);
-  }
-
-  static void printTeacherList(Teacher[] teachers, int length) {
-    for (int i = 0; i < length; i++) {
-      Teacher teacher = teachers[i];
-
-      System.out.printf("\n[%s]\n강의 분야: %s, 강의 경력: %s, 출간 저서: %s\n",
-      teacher.name,
-      teacher.language,
-      teacher.career,
-      teacher.writing);
     }
   }
 }
